55f7c81255f0f46f8d9b1436efa5fa51983976f0
Alexander Krieg <alitari67@gmail.com>
2018-12-27 20:06:58 +0100

    Add effort

--- springapp/src/main/java/de/alexkrieg/springapp/MessageController.java
+++ springapp/src/main/java/de/alexkrieg/springapp/MessageController.java
@@ -9,8 +9,17 @@ class MessageController {
 
     @GetMapping("/form")
     public String formGet(Model model) {
+        makeEffort();
         model.addAttribute("greet", "Hello from Spring!");
         return "message";
+    }
+
+    private void makeEffort() {
+        for (int i = 0; i < 1000000; i++) {
+            if (i % 100 == 0) {
+                System.out.print("," + i);
+            }
+        }
     }
 
 }
